a7ed0149e4a4a8f3e083e871bdebc7168b7e9c58
romina <pereyra.manrique@gmail.com>
2025-12-12 03:30:09 -0300

    Cambio mongoose.connect

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Conexión a MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("Mongo error:", err));
+
 
 // Schemas
 const userSchema = new mongoose.Schema({
